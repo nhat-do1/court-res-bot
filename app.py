7ca42f3ec18425d601a0bdcc90cf83cb8b7517a7
Nhat Do <ndo@4d.com>
2024-10-15 15:05:04 -0700

    Implement login

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
+# Selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+# testing
+import time
+
+# Environment
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
+#open website
 url = os.environ.get("URL")
 path = os.environ.get("DRIVER_PATH")
-browser = webdriver.Chrome(service=Service(path))
-browser.get(url)
+driver = webdriver.Chrome(service=Service(path))
+driver.get(url)
+
+#login
+email  = os.environ.get("EMAIL")
+pw = os.environ.get("PW")
+email_input = driver.find_element(By.ID, "Username")
+pw_input = driver.find_element(By.ID, "Password")
+login_button = driver.find_element(By.CSS_SELECTOR, "#loginForm > button")
+email_input.send_keys(email)
+pw_input.send_keys(pw)
+login_button.click()
+
+
