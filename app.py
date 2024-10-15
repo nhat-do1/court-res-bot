7260683a78d389015eae88ce49205ed263dbcb4c
Nhat Do <ndo@4d.com>
2024-10-15 16:51:31 -0700

    Refactor to go directly to res pg & log in only if needed

--- app.py
+++ app.py
@@ -11,20 +11,24 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-#open website
+# open website
 url = os.environ.get("URL")
-path = os.environ.get("DRIVER_PATH")
-driver = webdriver.Chrome(service=Service(path))
+driver_path = os.environ.get("DRIVER_PATH")
+profile_path = os.environ.get("PROFILE_PATH")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("user-data-dir=" + profile_path)
+# chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(service=Service(driver_path), options=chrome_options)
 driver.get(url)
 
-#login
-email  = os.environ.get("EMAIL")
-pw = os.environ.get("PW")
-email_input = driver.find_element(By.ID, "Username")
-pw_input = driver.find_element(By.ID, "Password")
-login_button = driver.find_element(By.CSS_SELECTOR, "#loginForm > button")
-email_input.send_keys(email)
-pw_input.send_keys(pw)
-login_button.click()
-
+# login if required
+if (len(driver.find_elements(By.LINK_TEXT, "Create Account")) > 0):
+    email  = os.environ.get("EMAIL")
+    pw = os.environ.get("PW")
+    email_input = driver.find_element(By.ID, "UserNameOrEmail")
+    pw_input = driver.find_element(By.ID, "Password")
+    login_button = driver.find_element(By.CSS_SELECTOR, "#loginForm > button")
+    email_input.send_keys(email)
+    pw_input.send_keys(pw)
+    login_button.click()
 
