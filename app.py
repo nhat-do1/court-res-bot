f4a2ffcd24de0230601228a521b0ade658aea234
Nhat Do <ndo@4d.com>
2024-10-24 12:47:00 -0700

    Check for bookable time slot & open reservation

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-# from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
 
 # testing
 import time as T
@@ -69,9 +68,32 @@ if (page_loaded):
             end_idx = data_str.find("&courtType")
             datetime_str = data_str[start_idx:end_idx]
             now = datetime.now()
-            print(datetime_str)
-            print(now.time())
-            print(now.date())
+            # print(datetime_str)  10/24/2024%209:30%20PM&end=10/24/2024%2010:00%20PM
+            # print(now.time())    11:15:44.657467
+            # print(now.date())    2024-10-24
+            today_str = str(now.date().strftime("%m/%d/%Y"))
+            datetime_parts = datetime_str.split("%20")
+            # print(datetime_parts)  ['10/24/2024', '9:30', 'PM&end=10/24/2024', '10:00', 'PM']
+            slot_date = datetime_parts[0]
+            slot_starttime_str = datetime_parts[1] + " " + datetime_parts[2][:2]
+            slot_starttime_obj = datetime.strptime(slot_starttime_str, '%I:%M %p').time()
+            dt = datetime.combine(now, slot_starttime_obj)
+            # not allowed to book time slots w/ start time w/i 30 min of current time 
+            if (today_str == slot_date and dt - now <= timedelta(minutes=30)):
+                continue
+            else:
+                # book slot
+                # later conditions: start time >= 6:30PM && end time >= start time + 1 HR
+                slot_button = driver.find_element(By.XPATH, f'//a[@data-href="{data_str}"]')
+                try:
+                    slot_button.click()
+                except StaleElementReferenceException:
+                    slot_button = driver.find_element(By.XPATH, f'//a[@data-href="{data_str}"]')
+                    slot_button.click()
+                break
+
+
+
         
 
 
